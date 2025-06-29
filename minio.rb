764ee78544cba9ab305e6635777191b18ef40a9a
Harshavardhana <harsha@minio.io>
2025-06-29 01:28:45 -0700

    update to latest AiStor minio.rb

--- minio.rb
+++ minio.rb
@@ -1,28 +1,27 @@
 class Minio < Formula
   # minio specific
-  git_tag = "RELEASE.2025-05-30T11-03-39Z"
+  git_tag = "RELEASE.2025-06-27T22-30-56Z"
 
   desc "High Performance Enterprise Object Storage compatible with Amazon S3 API"
-  homepage "https://min.io"
-  # url "https://github.com/minio/minio"
+  homepage "https://min.io/download?platform=macos"
   version git_tag
   revision 1
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://dl.min.io/aistor/minio/release/darwin-arm64/archive/minio.#{version}"
-      sha256 "421a8b464bd85059181af6a1c53223eb3965921f74908d0f5ae657098c48efd5"
+      sha256 "458c59fc9315cfeeed97bfcd9d0357ca51fd52f6b90db0cbf3d6ca47af141af4"
     else
       url "https://dl.min.io/aistor/minio/release/darwin-amd64/archive/minio.#{version}"
-      sha256 "0befe5494f981c84ac9d5b930e1f8462f94279636d71468b7a3b6655b108390a"
+      sha256 "f97954fc1c3474ebb2e45d280fc655679f1e5b5284be0f81163f0f64a09eb9cc"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://dl.min.io/aistor/minio/release/linux-arm64/archive/minio.#{version}"
-      sha256 "84dfb14b48e2ecebe8a829d67d05f29dd63f946fb911051d19801ebcc7a44d2d"
+      sha256 "f4abee5427a6bc50c6fa3a8bc1779770193be7833e61d425be019e4bfffd6363"
     else
       url "https://dl.min.io/aistor/minio/release/linux-amd64/archive/minio.#{version}"
-      sha256 "84ecfc4e9fa89c1e47d0758e3b45416f40dbd124b743f4e06e45b74155c85db8"
+      sha256 "0d0924e379348a62d01e76a9f0c7d8bb5c7eec8d04612b294fdb1dd51be8ac62"
     end
   end
 
@@ -34,57 +33,10 @@ class Minio < Formula
     ohai "Download complete!"
     ohai "Useful links:"
     puts <<~EOS
-      Command-line Access: https://docs.min.io/docs/minio-client-quickstart-guide
-
-      Object API (Amazon S3 compatible):
-         Go:         https://docs.min.io/docs/golang-client-quickstart-guide
-         Java:       https://docs.min.io/docs/java-client-quickstart-guide
-         Python:     https://docs.min.io/docs/python-client-quickstart-guide
-         JavaScript: https://docs.min.io/docs/javascript-client-quickstart-guide
-         .NET:       https://docs.min.io/docs/dotnet-client-quickstart-guide
-
-      Talk to the community: https://slack.min.io
+      Documentation: https://docs.min.io/enterprise/aistor-object-store/
+      Talk to MinIO Engineering Support: https://subet.min.io
     EOS
     ohai "Get started:"
     puts `#{bin}/minio server -h`
-  end
-
-  test do
-    (testpath/"config.json").write <<~EOS
-      {
-              "version": "14",
-              "credential": {
-                      "accessKey": "minio",
-                      "secretKey": "minio123"
-              },
-              "region": "us-east-1",
-              "browser": "on",
-              "logger": {
-                      "console": {
-                              "level": "error",
-                              "enable": true
-                      },
-                      "file": {
-                              "level": "error",
-                              "enable": false,
-                              "filename": ""
-                      }
-              },
-              "notify": {
-                      "redis": {
-                              "1": {
-                                      "enable": true,
-                                      "address": "127.0.0.1:6379",
-                                      "password": "",
-                                      "key": "minio_events"
-                              }
-                      }
-              }
-      }
-    EOS
-    minio_io = IO.popen("#{bin}/minio --config-dir #{testpath} server #{testpath}/export", :err=>[:child, :out])
-    sleep 1
-    Process.kill("INT", minio_io.pid)
-    assert_match("connection refused", minio_io.read)
   end
 end
